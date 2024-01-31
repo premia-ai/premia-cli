2829e9ec2603a935e76b42420beed06deba6a46a
ramin <rx.azhdari@gmail.com>
2024-01-31 14:40:28 +0100

    enh: update add_contracts template

--- templates/migrations/add_contracts.template.sql
+++ templates/migrations/add_contracts.template.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS contracts (
     symbol TEXT NOT NULL,
-    exercise_style TEXT NOT NULL,
     expiration_date TIMESTAMPTZ NOT NULL,
     underlying_ticker TEXT NOT NULL,
     contract_type TEXT NOT NULL,
     shares_per_contract INT NOT NULL,
-    strike_price DECIMAL NOT NULL
+    strike_price DECIMAL NOT NULL,
+    currency TEXT
 );
